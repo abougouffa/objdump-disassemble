4489939d1b99e437db2dbd7cb269e2f6d7c75d70
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-05-14 09:04:27 +0200

    refactor!: cleanup

---
+++ objdump-disassemble-utils.el
@@ -0,0 +1,48 @@
+;;; objdump-disassemble-utils.el --- Extra functions for `objdump'  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2025  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa <abougouffa@fedoraproject.org>
+;; Keywords: c, tools, files, languages
+
+;; SPDX-License-Identifier: MIT
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'objdump-disassemble-utils)
+
+
+(defcustom objdump-binary-predicate-chunk-size 1024
+  "The chunk size used in `objdump-binary-file-p' to check for binary files."
+  :group 'objdump-disassemble
+  :type '(choice file string))
+
+;; A predicate for detecting binary buffers. Inspired by: emacs.stackexchange.com/q/10277/37002
+;;;###autoload
+(defun objdump-binary-buffer-p (&optional buffer)
+  "Return whether BUFFER or the current buffer is binary.
+
+A binary buffer is defined as containing at least one null byte.
+
+Returns either nil, or the position of the first null byte."
+  (with-current-buffer (or buffer (current-buffer))
+    (save-excursion
+      (goto-char (point-min))
+      (search-forward (string ?\x00) nil t 1))))
+
+;;;###autoload
+(defun objdump-binary-file-p (file &optional chunk)
+  "Is FILE a binary?
+
+This checks the first CHUNK of bytes, defaults to 1024."
+  (with-temp-buffer
+    (insert-file-contents-literally file nil 0 (or chunk objdump-binary-predicate-chunk-size))
+    (objdump-binary-buffer-p)))
+
+
+(provide 'objdump-disassemble-utils)
+;;; objdump-disassemble-utils.el ends here
--- objdump-disassemble.el
+++ objdump-disassemble.el
@@ -1,9 +1,10 @@
 ;;; objdump-disassemble.el --- Disassemble executable and object files using objdump  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2024  Abdelhak Bougouffa
+;; Copyright (C) 2022-2025  Abdelhak Bougouffa
 
 ;; Author: Abdelhak Bougouffa <abougouffa@fedoraproject.org>
 ;; Keywords: c, tools, files, languages
+;; Version: 1.0
 
 ;; SPDX-License-Identifier: MIT
 
@@ -22,15 +23,12 @@
   "Use \"objdump\" to display disassembled executable and object files."
   :group 'tools)
 
-(defcustom objdump-executable "objdump"
+(defcustom objdump-disassemble-executable "objdump"
   "The \"objdump\" command."
   :group 'objdump-disassemble
   :type '(choice file string))
 
-(defcustom objdump-binary-predicate-chunk-size 1024
-  "The chunk size used in `objdump-binary-file-p' to check for binary files."
-  :group 'objdump-disassemble
-  :type '(choice file string))
+(make-obsolete-variable 'objdump-executable 'objdump-disassemble-executable "1.0")
 
 (defcustom objdump-disassemble-disable-on-remote nil
   "Don't try to disassemble remote files.
@@ -39,43 +37,20 @@ Normally, this requires objdump to be installed on the remote machine."
   :group 'objdump-disassemble
   :type 'boolean)
 
-;; A predicate for detecting binary buffers. Inspired by: emacs.stackexchange.com/q/10277/37002
-;;;###autoload
-(defun objdump-binary-buffer-p (&optional buffer)
-  "Return whether BUFFER or the current buffer is binary.
-
-A binary buffer is defined as containing at least one null byte.
-
-Returns either nil, or the position of the first null byte."
-  (with-current-buffer (or buffer (current-buffer))
-    (save-excursion
-      (goto-char (point-min))
-      (search-forward (string ?\x00) nil t 1))))
-
-;;;###autoload
-(defun objdump-binary-file-p (file &optional chunk)
-  "Is FILE a binary?
-
-This checks the first CHUNK of bytes, defaults to 1024."
-  (with-temp-buffer
-    (insert-file-contents-literally file nil 0 (or chunk objdump-binary-predicate-chunk-size))
-    (objdump-binary-buffer-p)))
-
 ;;;###autoload
 (defun objdump-recognizable-file-p (filename)
   "Can FILENAME be recognized by \"objdump\"."
   (when-let* ((file (and filename (file-truename filename)))
               (default-directory (file-name-directory filename)))
-    (and (executable-find objdump-executable)
+    (and (executable-find objdump-disassemble-executable)
          (or (not objdump-disassemble-disable-on-remote)
              (not (file-remote-p file)))
-         (file-exists-p file)
-         (not (file-directory-p file))
+         (file-regular-p file)
          (not (zerop (file-attribute-size (file-attributes file))))
          (not (string-match-p
                "file format not recognized"
                (shell-command-to-string
-                (format "%s --file-headers %s" objdump-executable (shell-quote-argument file))))))))
+                (format "%s --file-headers %s" objdump-disassemble-executable (shell-quote-argument file))))))))
 
 ;;;###autoload
 (defun objdump-recognizable-buffer-p (&optional buffer)
@@ -96,7 +71,7 @@ Return nil if the current buffer is not recognizable by objdump."
       (message "Disassembling %S using objdump." (file-name-nondirectory file))
       (erase-buffer)
       (set-visited-file-name (file-name-with-extension file ".objdump"))
-      (call-process objdump-executable nil (current-buffer) nil "-d" file)
+      (call-process objdump-disassemble-executable nil (current-buffer) nil "-d" file)
       (goto-char (point-min))
       (setq-local objdump-disassemble--orig-filename file))))
 
@@ -105,26 +80,13 @@ Return nil if the current buffer is not recognizable by objdump."
   (when (objdump-disassemble-buffer)
     (buffer-disable-undo)
     (set-buffer-modified-p nil)
-    (view-mode 1)
-    (read-only-mode 1)
     ;; Apply syntax highlighting from `asm-mode'
     (set-syntax-table objdump-disassemble-mode-syntax-table)
     (modify-syntax-entry ?# "< b") ; use # for comments
     (setq-local font-lock-defaults '(objdump-disassemble-font-lock-keywords))
-    (font-lock-update)))
-
-(defvar-local objdump-symbol-table nil)
-(defun objdump--get-symbols ()
-  (or objdump-symbol-table
-      (progn
-        (setq objdump-symbol-table (obarray-make 300))
-        (save-excursion
-          (save-match-data
-            (goto-char (point-min))
-            (while (re-search-forward "^[0-9a-f]+ <\\([a-zA-Z_0-9:.]+\\)>:$"
-                                      nil t)
-              (intern (match-string 1) objdump-symbol-table))))
-        objdump-symbol-table)))
+    (font-lock-update)
+    (view-mode 1)
+    (read-only-mode 1)))
 
 (defun objdump-disassemble-teardown ()
   "Setup the current buffer for `objdump-disassemble-mode'."
@@ -142,13 +104,16 @@ Return nil if the current buffer is not recognizable by objdump."
       (objdump-disassemble-setup)
     (objdump-disassemble-teardown)))
 
+(defun objdump-disassemble-enable ()
+  (objdump-disassemble-mode 1))
+
 ;;;###autoload
-(define-global-minor-mode global-objdump-disassemble-mode objdump-disassemble-mode objdump-disassemble-setup
+(define-global-minor-mode global-objdump-disassemble-mode objdump-disassemble-mode objdump-disassemble-enable
   :lighter "Objdump"
   :group 'objdump-disassemble
   (if global-objdump-disassemble-mode
-      (add-hook 'magic-fallback-mode-alist '(objdump-binary-buffer-p . objdump-disassemble-mode) 99)
-    (cl-callf2 delete '(objdump-binary-buffer-p . objdump-disassemble-mode) magic-fallback-mode-alist)))
+      (add-hook 'magic-fallback-mode-alist '(objdump-recognizable-buffer-p . objdump-disassemble-mode) 99)
+    (cl-callf2 delete '(objdump-recognizable-buffer-p . objdump-disassemble-mode) magic-fallback-mode-alist)))
 
 
 (provide 'objdump-disassemble)
